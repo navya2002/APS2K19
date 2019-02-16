1fc146eb6f688e10306011fe1d4b0a5e12fb5cbc
navya2002 <35269051+navya2002@users.noreply.github.com>
2019-02-17 00:13:18 +0530

    Update powerset.c

--- Day_today_codes/Bit_Manipulation/powerset.c
+++ Day_today_codes/Bit_Manipulation/powerset.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
+
+void  powerset(char a[],int set_size)
+{
+
+	int size=pow(2,set_size);
+	int i,j;
+	for(i=0;i<size;i++)
+	{
+		for(j=0;j<set_size;j++)
+		{
+			if(i  & (1 << j))
+			{
+				printf("%c ",a[j]);
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	char a[3]={'a','b','c'};
+	powerset(a,3);
+	return 0;
+}
