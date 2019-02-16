a1aa767c7b7ba8ca1419870c5c07816577747ed1
navya2002 <35269051+navya2002@users.noreply.github.com>
2019-02-16 23:03:07 +0530

    Update countsetbits.c

--- Day_today_codes/Bit_Manipulation/countsetbits.c
+++ Day_today_codes/Bit_Manipulation/countsetbits.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<limits.h>
 
+int count_set_bits(int n)
+{
+	
+	 int c,count=0;
+	 while(n)
+	 {
+	 	c=n & 1;
+	 	if(c)
+	 		count++;
+	 	n=n >> 1;
+	 }
+
+	 return count;
+	
+}
+
+
+
+int main()
+{
+	printf("%d",count_set_bits(7));
+	return 0;
+}
